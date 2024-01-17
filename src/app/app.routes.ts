85756e6cb613e888127fbb4a1b152b14a9ec9281
daniel <danielpessoa432@gmail.com>
2024-01-17 14:45:24 -0300

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [{
-  path: '',
-  redirectTo: 'rules',
-  pathMatch: 'full'
-},
-{
-  path: 'rules',
-  loadChildren: () => import('./rules/rules.module').then(m => m.RulesModule)
-}
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'rules',
+    pathMatch: 'full'
+  },
+  {
+    path: 'rules',
+    loadChildren: () => import('./rules/rules.module').then(m => m.RulesModule)
+  },
+  {
+    path: 'upload',
+    loadChildren: () => import('./upload/upload.module').then(m => m.UploadModule)
+  },
+  {
+    path: 'results',
+    loadChildren: () => import('./results/results.module').then(m => m.ResultsModule)
+  }
 ];
