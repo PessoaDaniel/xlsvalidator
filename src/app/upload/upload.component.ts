a22ec2652aa5abe795297cacab382e62e0e4b250
daniel <danielpessoa432@gmail.com>
2024-01-20 17:52:13 -0300

    upload lib

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,16 +1,26 @@
 import {Component} from '@angular/core';
 import {RulesService} from "../shared/services/rules.service";
+import {DropzoneCdkModule} from "@ngx-dropzone/cdk";
+import {DropzoneMaterialModule} from "@ngx-dropzone/material";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-upload',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    DropzoneCdkModule,
+    DropzoneMaterialModule,
+    MatIconModule
+  ],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.scss'
 })
 export class UploadComponent {
   rules: Array<any> = [];
-
+  accept = ' application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel'
   constructor(private rulesService: RulesService) {}
   ngOnInit() {
     this.rulesService.rules.subscribe((rule) => {
