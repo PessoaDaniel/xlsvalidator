7bae4bd8cbf47e46d159532a3514c0f1c2b473d1
daniel <danielpessoa432@gmail.com>
2024-01-20 15:56:57 -0300

    rule card

--- src/app/rules/rules.component.ts
+++ src/app/rules/rules.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-rules',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './rules.component.html',
   styleUrl: './rules.component.scss'
 })
 export class RulesComponent {
+  rules: Array<Object> = [];
 
+  addRule() {
+    this.rules.push({});
+  }
 }
