f7a17be0d4da89117fbd18906e458c99e012a0b5
daniel <danielpessoa432@gmail.com>
2024-01-21 10:27:24 -0300

    form upload config

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,9 +1,11 @@
 import {Component} from '@angular/core';
 import {RulesService} from "../shared/services/rules.service";
-import {DropzoneCdkModule} from "@ngx-dropzone/cdk";
+import {DropzoneCdkModule, FileInputValidators} from "@ngx-dropzone/cdk";
 import {DropzoneMaterialModule} from "@ngx-dropzone/material";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatIconModule} from '@angular/material/icon';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MatChipRow} from "@angular/material/chips";
 
 
 @Component({
@@ -13,15 +15,26 @@ import {MatIconModule} from '@angular/material/icon';
     MatFormFieldModule,
     DropzoneCdkModule,
     DropzoneMaterialModule,
-    MatIconModule
+    MatIconModule,
+    ReactiveFormsModule,
+    MatChipRow
   ],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.scss'
 })
 export class UploadComponent {
   rules: Array<any> = [];
-  accept = ' application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel'
-  constructor(private rulesService: RulesService) {}
+  form: FormGroup;
+  private finputValidatos =  [
+    FileInputValidators.accept('.xls')
+  ]
+  constructor(
+    private rulesService: RulesService,
+    private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      uploadedFile: [null, this.finputValidatos]
+    });
+  }
   ngOnInit() {
     this.rulesService.rules.subscribe((rule) => {
       if (rule) {
@@ -29,4 +42,5 @@ export class UploadComponent {
       }
     });
   }
+  clear() {}
 }
