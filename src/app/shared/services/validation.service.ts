eb51f9ec3175e540269ae11f69d3769ecac5defb
daniel <danielpessoa432@gmail.com>
2024-01-21 20:32:27 -0300

    column key validation

---
+++ src/app/shared/services/validation.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import {Rule} from "../../../models/Rule";
+import {ValidationError} from "../../../models/ValidationError";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidationService {
+  constructor() { }
+
+  validateKeys(fileData: Array<any>, rules: Rule[]) {
+    let fileKeys = fileData[0];
+    let validationErrors:Array<ValidationError> = [];
+    let count = 0;
+    for (const fileKey of fileKeys) {
+      count = count + 1;
+      console.log(fileKey);
+      if (!fileKey || fileKey == '' || Number.isNaN(fileKey)) {
+        let error = new ValidationError(`Chave vazia detectada na linha 1 coluna ${count}`);
+        error.type = 'error';
+        validationErrors.push(error);
+      }
+    }
+    for (const rule of rules) {
+      if(rule.key) {
+        if (!fileKeys.includes(rule.key.trim())) {
+          let error = new ValidationError(`Chave não localizada no arquivo`);
+          error.field = rule.key;
+          if (rule.required) {
+            error.message = 'Chave obrigatória não localizada no arquivo';
+            error.type = 'error';
+          }
+          validationErrors.push(error);
+        }
+      }
+    }
+
+    if (validationErrors.length) {
+      return validationErrors;
+    }  else {
+      return null;
+    }
+  }
+  store(errors: ValidationError[]) {
+    window.localStorage.setItem('RESULTS', JSON.stringify(errors));
+  }
+
+  getStored(): ValidationError[] {
+    if (window.localStorage.getItem('RESULTS')) {
+      // @ts-ignore
+      return JSON.parse(window.localStorage.getItem('RESULTS'));
+    } else {
+      return  [];
+    }
+  }
+}
